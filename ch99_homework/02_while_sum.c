ab9c23322c38d1402b19ddfeb300647359766825
Kty825221 <xodud97124@naver.com>
2025-01-10 11:25:32 +0900

    Add Lecture07

--- ch99_homework/02_while_sum.c
+++ ch99_homework/02_while_sum.c
@@ -4,5 +4,23 @@
 //        홀수합, 짝수합 계산
 
 int main() {
+    int num = 1;
+    int evenNum;  // 짝수 합 저장할 변수
+    int oddNum;  // 홀수 합 저장할 변수
+
+    while(num <= 100) {
+        if(num % 2 == 0) {
+            evenNum += num;
+        }
+        else {
+            oddNum += num;
+        }
+
+        num++;
+    }
+
+    printf("짝수의 합은 %d입니다.\n", evenNum);
+    printf("홀수의 합은 %d입니다.\n", oddNum);
     
+
 }
